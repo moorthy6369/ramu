5e8220b4fef6a9b9bdba008e9a8b821dd1061786
www.ramamoorthy6374@gmail.com <www.ramamoorthy6374@gmail.com>
2023-08-25 22:56:24 +0530

    v1.1

--- main.js
+++ main.js
@@ -1,70 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-      // Get the current URL, removing any fragment
-      var documentUrl = document.location.href.replace(/#.*$/, '')
-
-      // Iterate through all links
-      var linkEls = document.getElementsByTagName('A')
-      for (var linkIndex = 0; linkIndex < linkEls.length; linkIndex++) {
-        var linkEl = linkEls[linkIndex]
-
-        // Ignore links that don't begin with #
-        if (!linkEl.getAttribute('href').match(/^#/)) {
-          continue;
-        }
-
-        // Convert to an absolute URL
-        linkEl.setAttribute('href', documentUrl + linkEl.getAttribute('href'))
-      }
-    })
-
-//slider 
-
 const slider = document.querySelector('.slider');
-const sliderImages = slider.querySelectorAll('img');
-//const prevButton = document.querySelector('.prev-btn');
-//const nextButton = document.querySelector('.next-btn');
-let count = 0;
+const slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
-function slideNext() {
-  count++;
-  if (count >= sliderImages.length) {
-    count = 0;
-  }
-  updateSlider();
+function showSlide(index) {
+  slider.style.transform = `translateX(-${index * 100}%)`;
 }
 
-function slidePrev() {
-  count--;
-  if (count < 0) {
-    count = sliderImages.length - 1;
-  }
-  updateSlider();
+function updateIndicator(index) {
+  const dots = indicator.querySelectorAll('.dot');
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
 }
 
-function updateSlider() {
-  const offset = -count * slider.offsetWidth;
-  slider.style.transform = `translateX(${offset}px)`;
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
 }
 
 slider.addEventListener('touchstart', (e) => {
   touchStartX = e.touches[0].clientX;
 });
 
-slider.addEventListener('touchmove', (e) => {
-  touchMoveX = e.touches[0].clientX;
-});
-
-slider.addEventListener('touchend', () => {
-  const diffX = touchMoveX - touchStartX;
-  if (diffX > 50) {
-    slidePrev();
-  } else if (diffX < -50) {
-    slideNext();
+slider.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].clientX;
+  if (touchStartX - touchEndX > 50) {
+    // Swipe left
+    nextSlide();
+  } else if (touchEndX - touchStartX > 50) {
+    // Swipe right
+    prevSlide();
   }
 });
 
-//nextButton.addEventListener('click', slideNext);
-//prevButton.addEventListener('click', slidePrev);
+setInterval(nextSlide, 3000); // Change image every 3 seconds
 
-// Automatically loop the slider
-setInterval(slideNext, 3000); // Change interval time as desired
+
